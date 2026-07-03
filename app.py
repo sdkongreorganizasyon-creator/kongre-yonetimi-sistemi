74a276bc2aa13b8d5c701ca807938efa2b48ce07
sdkongreorganizasyon-creator <sdkongreorganizasyon@gmail.com>
2026-07-03 10:23:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="Kongre Yönetimi Sistemi",
+    page_title="Süreç Yönetimi Sistemi",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="collapsed",
