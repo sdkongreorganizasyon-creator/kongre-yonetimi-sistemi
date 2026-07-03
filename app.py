9f505e2cfca0be4c7b71d767e0b2c434f81b00ce
sdkongreorganizasyon-creator <sdkongreorganizasyon@gmail.com>
2026-07-03 13:55:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="Süreç Yönetimi Sistemi",
+    page_title="EVENTIX",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="collapsed",
